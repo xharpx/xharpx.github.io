b8c0a0e4bf8d80459fbc4b7098e1094b290b5757
xharpx <144825028+xharpx@users.noreply.github.com>
2023-12-12 14:11:38 -0500

    Add files via upload

---
+++ projects/chapter10/apply/scripts/script.js
@@ -0,0 +1,12 @@
+/*
+    Student Name: Harpreet Kaur
+    Date: 12/12/23
+    File Name: script.js
+*/
+
+function content() {
+    var text = document.getElementById("new");
+    text.textContent = "Hi. I am a new paragraph that was created with a JavaScript function.";
+    text.style.color = "#c0145f";
+    text.style.fontSize = "2em";
+}
