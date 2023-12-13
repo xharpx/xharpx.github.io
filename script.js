ecda7a66792152184e943009d945c6e7c54cdada
xharpx <144825028+xharpx@users.noreply.github.com>
2023-12-13 14:08:38 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+/*
+Harpreet Kaur
+File Name: script.js
+Date: 12/13/23
+*/
+
+//hamburger menu function
+function hamburger() {
+    var menu = document.getElementById("menu-links");
+    var logo = document.getElementById("ffc-logo");
+    if (menu.style.display === "block" && logo.style.display === "none") {
+        menu.style.display = "none";
+        logo.style.display = "block";
+    } else {
+        menu.style.display = "block";
+        logo.style.display = "none";
+    }
+}
