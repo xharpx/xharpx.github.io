30f0805108c9c1e1a80df2b6f5b1d4323d6fb761
xharpx <144825028+xharpx@users.noreply.github.com>
2024-04-04 13:05:59 -0400

    Add files via upload

---
+++ projects/javascript/chapter01/project02/project01-02_txt.js
@@ -0,0 +1,11 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-2
+
+    Author: Harpreet Kaur
+    Date:   4/4/2024
+
+    Filename: project01-02.js
+*/
+
+//define variables for service name and service speed
