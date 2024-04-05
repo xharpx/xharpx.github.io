0cc0189a9945f1762289096a34abe3a790e83b84
xharpx <144825028+xharpx@users.noreply.github.com>
2024-04-05 12:41:51 -0400

    Add files via upload

---
+++ projects/javascript/chapter01/project04/project01-04_txt.js
@@ -0,0 +1,12 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-4
+
+    Author: Harpreet Kaur
+    Date:   4/5/2024
+
+    Filename: project01-04.js
+*/
+
+//define variables for home and work addresses
+
