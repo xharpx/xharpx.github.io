c137e0d485b81902fd2abcdeee24dab7a7588448
xharpx <144825028+xharpx@users.noreply.github.com>
2024-04-24 14:23:28 -0400

    Add files via upload

---
+++ projects/javascript/chapter03/chapter/js03_txt.js
@@ -0,0 +1,14 @@
+/*    JavaScript 7th Edition
+     Chapter 3
+     Chapter case
+
+     Tipton Turbines
+     Program to display games results in a web table
+     Author: Harpreet Kaur  
+     Date:   4/19/24
+
+     Filename: js03.js
+ */
+
+//days of the week
+let weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
